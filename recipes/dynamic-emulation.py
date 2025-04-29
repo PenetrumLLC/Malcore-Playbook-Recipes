a032e5794a4b5156b7776d3efa7185a90e7bff0f
Ekultek <14183473+Ekultek@users.noreply.github.com>
2025-04-29 13:09:00 -0500

    updates

--- recipes/dynamic-emulation.py
+++ recipes/dynamic-emulation.py
@@ -3,13 +3,13 @@ import malcore_playbook.lib.settings as settings
 
 
 __version__ = "0.1"
-__hashsum__ = "3be6e99cdbcd7056a76515cfe595aa20"
+__hashsum__ = "8311e49cce322aef0102c929aa00fda2"
 __author__ = "Thomas Perkins"
 __excluded_plans__ = "free"
 
 
 def plugin(*args, **kwargs):
-    results = ""
+    results = None
     try:
         filename = args[0][0]
     except:
@@ -21,12 +21,7 @@ def plugin(*args, **kwargs):
         api_ = api.Api()
         req = api_.upload_file(filename, "dynamicanalysis")
         data = req['data']['data'][0]['dynamic_analysis']
-        for part in data:
-            eps = part['entry_points']
-            for ep in eps:
-                apis = ep['apis']
-                for _api in apis:
-                    results += f"{_api['pc']}: {_api['api_name']}({','.join(_api['args'])})->{_api['ret_val']}\n"
+        results = data
         return results
     except:
         return results
